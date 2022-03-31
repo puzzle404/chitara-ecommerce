d9dd147961f163c7728170ae85ecf65822d411b5
Manuel Ferrer <cmanuferrer@gmail.com>
2022-03-30 21:39:15 -0300

    Se hicieron correcciones para que sea responsive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @sandals = Sandal.first(3).reverse
+    @sandals = Sandal.first(4).reverse
 
     @sandal1 = @sandals[0]
     @sandal2 = @sandals[1]
     @sandal3 = @sandals[2]
+    @sandal4 = @sandals[2]
+
   end
 end
